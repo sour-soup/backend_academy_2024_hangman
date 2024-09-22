ff3b6ba79df035a4aef0946ca666061e1f400003
sour-soup <kiziyepi8@gmail.com>
2024-09-23 03:49:54 +0400

    feat: add DependencyResolver and ServiceNotRegisteredException

---
+++ src/main/java/backend/academy/hangman/config/DependencyResolver.java
@@ -0,0 +1,41 @@
+package backend.academy.hangman.config;
+
+import backend.academy.hangman.exception.ServiceNotRegisteredException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class DependencyResolver {
+    private final Map<Class<?>, Supplier<?>> suppliers = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+
+    private DependencyResolver() {
+    }
+
+    public <T> void register(Class<T> serviceClass, Supplier<T> supplier) {
+        suppliers.put(serviceClass, supplier);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T resolve(Class<T> serviceClass) {
+        Object instance = instances.get(serviceClass);
+        if (instance != null) {
+            return (T) instance;
+        }
+
+        var supplier = suppliers.get(serviceClass);
+        if (supplier == null) {
+            throw new ServiceNotRegisteredException("No implementation registered for " + serviceClass.getName());
+        }
+
+        T newInstance = (T) supplier.get();
+        instances.put(serviceClass, newInstance);
+        return newInstance;
+    }
+
+    private static final DependencyResolver INSTANCE = new DependencyResolver();
+
+    public static DependencyResolver getInstance() {
+        return INSTANCE;
+    }
+}
