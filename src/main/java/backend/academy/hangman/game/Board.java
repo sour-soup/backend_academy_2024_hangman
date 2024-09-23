44dc450ae3a6b840a1a36ca9be1b7b0b155e5d10
sour-soup <kiziyepi8@gmail.com>
2024-09-23 21:49:15 +0400

    feat: add Board class with unit tests and board.txt resource file

---
+++ src/main/java/backend/academy/hangman/game/Board.java
@@ -0,0 +1,92 @@
+package backend.academy.hangman.game;
+
+import backend.academy.hangman.exception.ResourceLoadingException;
+import backend.academy.hangman.utils.RandomUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Board {
+    private static final String LOAD_ERROR_MESSAGE = "Failed to load board data: ";
+    private static final double START_PART = 0.1;
+
+    private String asciiArt;
+    private boolean[] activeSymbols;
+
+    public Board() {
+        this("");
+    }
+
+    public Board(String asciiArt) {
+        initializeAsciiArt(asciiArt);
+    }
+
+    public void loadAsciiArt(String asciiArt) {
+        initializeAsciiArt(asciiArt);
+    }
+
+    public void loadDefaultAsciiArt() {
+        try (InputStream defaultArtStream = getClass().getClassLoader().getResourceAsStream("game/board.txt")) {
+            loadAsciiArt(Objects.requireNonNull(defaultArtStream));
+        } catch (IOException e) {
+            throw new ResourceLoadingException(LOAD_ERROR_MESSAGE + e.getMessage(), e);
+        }
+    }
+
+    public void loadAsciiArt(InputStream asciiArtStream) {
+        try {
+            initializeAsciiArt(new String(asciiArtStream.readAllBytes(), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new ResourceLoadingException(LOAD_ERROR_MESSAGE + e.getMessage(), e);
+        }
+    }
+
+    public String getAsciiArt() {
+        return IntStream.range(0, asciiArt.length())
+            .mapToObj(i -> (activeSymbols[i] || Character.isWhitespace(asciiArt.charAt(i)))
+                ? String.valueOf(asciiArt.charAt(i)) : " ")
+            .collect(Collectors.joining());
+    }
+
+    public void go(long currentAttempts, long maxAttempts) {
+        long totalSymbols = countNonSpaceSymbols();
+        long activeSymbolsCount = IntStream.range(0, activeSymbols.length).filter(i -> activeSymbols[i]).count();
+
+        double percentage = START_PART + (double) currentAttempts / maxAttempts * (1 - START_PART);
+        long expectedActiveSymbols = currentAttempts == maxAttempts ? totalSymbols : (long) (totalSymbols * percentage);
+        long symbolsToActivate = expectedActiveSymbols - activeSymbolsCount;
+
+        activateSymbols(symbolsToActivate);
+    }
+
+    public void clear() {
+        activeSymbols = new boolean[asciiArt.length()];
+        activateSymbols((long) (countNonSpaceSymbols() * START_PART));
+    }
+
+    private void initializeAsciiArt(String asciiArt) {
+        this.asciiArt = asciiArt;
+        this.activeSymbols = new boolean[asciiArt.length()];
+        activateSymbols((long) (countNonSpaceSymbols() * START_PART));
+    }
+
+    private long countNonSpaceSymbols() {
+        return asciiArt.chars()
+            .filter(ch -> !Character.isWhitespace(ch))
+            .count();
+    }
+
+    private void activateSymbols(long n) {
+        List<Integer> inactiveSymbols = IntStream.range(0, asciiArt.length())
+            .filter(i -> !Character.isWhitespace(asciiArt.charAt(i)))
+            .filter(i -> !activeSymbols[i])
+            .boxed().toList();
+
+        RandomUtils.getRandomElements(inactiveSymbols, n)
+            .forEach((i) -> activeSymbols[i] = true);
+    }
+}
---
+++ src/test/java/backend/academy/hangman/game/BoardTest.java
@@ -0,0 +1,73 @@
+package backend.academy.hangman.game;
+
+import backend.academy.hangman.exception.ResourceLoadingException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    void loadAsciiArt_ShouldLoadDataCorrectly_WhenStringIsGiven() {
+        // Arrange
+        String art = "ART";
+
+        // Act
+        board.loadAsciiArt(art);
+
+        // Assert
+        assertThat(board.getAsciiArt().length()).isEqualTo(art.length());
+    }
+
+    @Test
+    void loadAsciiArt_ShouldLoadData_WhenInputStreamIsCorrectly() {
+        // Arrange
+        String art = "ART";
+        InputStream inputStream = new ByteArrayInputStream(art.getBytes());
+
+        // Act
+        board.loadAsciiArt(inputStream);
+
+        // Assert
+        assertThat(board.getAsciiArt().length()).isEqualTo(art.length());
+    }
+
+    @Test
+    void loadAsciiArt_ShouldThrowException_WhenInputStreamIsNull() {
+        // Arrange
+        try (InputStream inputStreamMock = mock(InputStream.class)) {
+            when(inputStreamMock.readAllBytes()).thenThrow(new IOException(""));
+
+            // Act & Assert
+            assertThatThrownBy(() -> board.loadAsciiArt(inputStreamMock))
+                .isInstanceOf(ResourceLoadingException.class)
+                .hasMessageContaining("Failed to load board data: ");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void go_ShouldActivateAllSymbols_AfterMaxAttempts() {
+        String art = "ART";
+        board.loadAsciiArt(art);
+
+        // Act
+        board.go(5, 5);
+
+        // Assert
+        assertThat(board.getAsciiArt()).isEqualTo(art);
+    }
+}
